8f5b78a72eac241588db8b0d58b77403f7ba11d8
wren_pi <welihayes@gmail.com>
2024-01-20 19:54:14 -0500

    setting up pages for playstory module

--- textgamebuilder/playstory/views.py
+++ textgamebuilder/playstory/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home_view(request, *args, **kwargs):
+    context = {}
+    return render(request, "home.html", context)
--- textgamebuilder/textgamebuilder/urls.py
+++ textgamebuilder/textgamebuilder/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from playstory.views import home_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_view, name='home'),
 ]
