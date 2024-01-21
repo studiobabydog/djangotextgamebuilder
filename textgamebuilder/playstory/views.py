342684bf8ab4ad11a9b11c8ee8a66e4a0bf33f72
wren_pi <welihayes@gmail.com>
2024-01-20 20:10:31 -0500

    adding models for story choices

--- textgamebuilder/playstory/views.py
+++ textgamebuilder/playstory/views.py
@@ -1,6 +1,44 @@
 from django.shortcuts import render
+from storyblock.models import Story, StoryChoice
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
     context = {}
     return render(request, "home.html", context)
+
+def story_view(request, storyslug, *args, **kwargs):
+    story = Story.objects.get(story_slug=storyslug)
+    story_choice = StoryChoice.objects.filter(story_id=story.story_id, choice_id=1).first()
+    context = {
+        'story_choice': story_choice,
+        }
+    return render(request, "play_story.html", context)
+
+def validate_story_choice(request, story_choice, *args, **kwargs):
+    if request.POST.get('choice') == 'next1':
+        story_do = story_choice.choice_id + story_choice.next_choice1_add
+    elif request.POST.get('choice') == 'next2':
+        story_do = story_choice.choice_id + story_choice.next_choice2_add
+    elif request.POST.get('choice') == 'back':
+        story_do = story_choice.choice_id - story_choice.prev_choice_sub
+    elif request.POST.get('choice') == 'continue':
+        story_do = StoryChoice.objects.filter(story_id = story_choice.story_id, choice_id=request.POST.get('continue_choice')).first().choice_id
+    return story_do
+
+def do_story(request, *args, **kwargs):
+    if request.method == "POST":
+        # Log story
+        story = story.objects.filter(story_name=request.POST.get('story_id')).first()
+        story_choice = StoryChoice.objects.filter(story_id=story.story_id, choice_id=request.POST.get('choice_id')).first()
+        return_story = validate_story_choice(request, story_choice)
+        continue_story_choice = StoryChoice.objects.filter(story_id=story.story_id, choice_id=return_story).first()
+        final_story_choice = StoryChoice.objects.all().order_by('-choice_id').first()
+        chkpt_message = ''     
+        context = {
+            'final_story_choice': final_story_choice,
+            'story_choice': continue_story_choice
+            }
+
+        return render(request, "do_choice.html", context)
+    elif request.method == "GET":
+        return render(request, "home.html")
--- textgamebuilder/storyblock/models.py
+++ textgamebuilder/storyblock/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class Story(models.Model):
+	story_id = models.BigAutoField(auto_created=True, primary_key=True, verbose_name="story_id")
+	story_name = models.CharField(max_length=35, verbose_name="story_name")
+	story_isactive = models.BooleanField(default=True, verbose_name="story_is_active")
+	story_slug = models.SlugField(null=False, verbose_name="story_slug", unique=True)
+
+	def __str__(self):
+		return str(f'{self.story_name}')
+	
+	def get_absolute_url(self):
+		return reverse("play-story", kwargs={"storyslug": self.story_slug})
+	
+class StoryChoice(models.Model):
+	story_id = models.ForeignKey(Story, on_delete=models.CASCADE)
+	choice_id = models.PositiveIntegerField(default=1)
+	choice_image = models.URLField(default='https://via.placeholder.com/150')
+	choice_image_alt = models.TextField(default='image', max_length=255, verbose_name="choice_image_alt_text")
+	choice_text = models.TextField()
+	prev_choice_sub = models.PositiveIntegerField(default=1)
+	prev_choice_txt = models.CharField(max_length=32, default='return')
+	next_choice1_add = models.PositiveIntegerField(default=1)
+	next_choice1_txt = models.CharField(max_length=32, default='choice 1')
+	next_choice2_add = models.PositiveIntegerField(default=1)
+	next_choice2_txt = models.CharField(max_length=32, default='choice 2')
+	is_checkpoint = models.BooleanField(default=False)
+	class Meta:
+		unique_together = ["story_id", "choice_id"]
+	
+	def __str__(self):
+			return str(f'adv-{self.story_id}-choice-{self.choice_id}')
